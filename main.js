fed51618b00a0b504ddd34b729f82bf9bf8b7783
pietasty <randomalexwu@gmail.com>
2017-02-26 21:25:04 +0800

    prototype of clickable social media links in the banner of website

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import Body from './components/body.jsx';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import IconButton from 'material-ui/IconButton';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -15,10 +16,22 @@ class App extends React.Component {
             'maxWidth': "100%",
             margin: "auto"
         };
+
+        var socialMedia = (
+            <div>
+                <IconButton iconClassName="fa fa-facebook-official" />
+                <IconButton iconClassName="fa fa-snapchat" />
+                <IconButton iconClassName="fa fa-instagram" />
+                <IconButton iconClassName="fa fa-twitter" />
+            </div>
+        );
+
         return (
             <MuiThemeProvider>
                 <Card className="container" style={styles}>
-                    <CardMedia>
+                    <CardMedia
+                        overlay={socialMedia}
+                    >
                         <img src="./images/FB-Banner-Final.jpg"/>
                     </CardMedia>
                     <CardActions>
