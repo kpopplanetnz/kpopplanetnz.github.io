9a5d8a1a7962ba1faac18575db01163a2d6db5b7
Jack Lin <jacklin213@gmail.com>
2017-03-02 00:57:45 +1300

    Revert commit b620c9

--- components/sponsors.jsx
+++ components/sponsors.jsx
@@ -1,75 +1,48 @@
 import React from 'react';
 import {GridList, GridTile} from 'material-ui/GridList';
-import {List, ListItem} from 'material-ui/List';
-import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 const styles = {
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        color: 'gray',
-    },
-    gridList: {
-        width: 500,
-        height: 450,
-        overflowY: 'auto',
-    },
-    card: {
-        flexBasis: '70%',
-    },
-    list: {
-        flexBasis: '30%',
-        backgroundColor: '#e5e5e5',
-    },
-    sponsor: {
-        padding: 10,
-        textAlign: 'right',
-    },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    overflowY: 'auto',
+  },
 };
 
 const tilesData = [
-    {
-        img: './images/FB-Banner-Final.jpg',
-        title: 'Sponsor 1',
-        //author: 'jill111',
-        active: true
-    },
-    {
-        img: './images/FB-Banner-Final.jpg',
-        title: 'Sponsor 2',
-    }
+  {
+    img: './images/FB-Banner-Final.jpg',
+    title: 'Sponsor 1',
+    //author: 'jill111',
+  },
+  {
+    img: './images/FB-Banner-Final.jpg',
+    title: 'Sponsor 2',
+  }
 ]
 
 const Sponsors = () => (
     <div style={styles.root}>
-        <List style={styles.list}>
-            {/*<ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />*/}
-            <ListItem primaryText="Sponsor 1" />
-            <ListItem primaryText="Sponsor 2" />
-        </List> 
-        {tilesData.map((tile) => (
-            <div 
-                key={tile.title}
-                className={tile.active ? "active" : "hidden"}
-                style={styles.card}
-            >
-                <Sponsor title={tile.title}></Sponsor>
-            </div>
-        ))}
+    <GridList
+      cellHeight={180}
+      style={styles.gridList} >
+      {tilesData.map((tile) => (
+        <GridTile
+          key={tile.title}
+          title={tile.title}
+          //subtitle={<span>by <b>{tile.author}</b></span>}
+          //actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+        >
+          <img src={tile.img} />
+        </GridTile>
+      ))}
+    </GridList>
   </div>
 )
-
-function Sponsor(props) {
-    return (
-        <div style={styles.sponsor}>
-            <h1>{props.title}</h1>
-        </div>
-    )
-}
-
-function toggleActive(props) {
-    tilesData[props.primaryText].active = !tile.active
-}
 
 export default Sponsors;
