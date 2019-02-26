0a7cf04d23051527509eca46c83d69c25a60623e
Jack Lin <jacklin213@gmail.com>
2019-02-26 13:53:21 +1100

    Implement url routes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import Home from './Home';
 import Sponsors from './Sponsors';
@@ -6,24 +7,35 @@ import AboutUs from './AboutUs';
 import Events from './Events';
 import ContactUs from './ContactUs';
 
-const Body = () => (
-    <Tabs>
-        <Tab label="Home">
-            <Home/>
-        </Tab>
-        <Tab label="Events">
-            <Events/>
-        </Tab>
-        <Tab label="Sponsors">
-            <Sponsors/>
-        </Tab>
-        <Tab label="About Us">
-            <AboutUs/>
-        </Tab>
-        {/* <Tab label="Contact Us">
-            <ContactUs/>
-        </Tab> */}
-    </Tabs>
-);
+class Body extends React.Component {
+    handleCallToRouter = (value) => {
+        this.props.history.push(value);
+    }
+     
+    render() {
+        return(
+            <Tabs
+                value={this.props.history.location.pathname}
+                onChange={this.handleCallToRouter}
+                >
+                `<Tab label="Home" value="/">
+                    <Home/>
+                </Tab>
+                <Tab label="Events" value="/events">
+                    <Events/>
+                </Tab>
+                <Tab label="Sponsors" value="/sponsors">
+                    <Sponsors/>
+                </Tab>
+                <Tab label="About Us" value="/aboutus">
+                    <AboutUs/>
+                </Tab>
+                {/* <Tab label="Contact Us">
+                    <ContactUs/>
+                </Tab> */}
+            </Tabs>
+        );
+    }
+}
 
-export default Body;
+export default withRouter(Body);
