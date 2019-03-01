6b8a2bfee4aa910bc6a38e7a37ed75b590e43b7e
Jack Lin <jacklin213@gmail.com>
2019-03-01 18:20:03 +1100

    Update Sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,42 +1,37 @@
 import React from 'react';
-import GridList from '@material-ui/core/GridList';
-import {Card, CardMedia, Typography} from '@material-ui/core';
+import {Card, CardContent, CardMedia, Grid, Typography} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles' 
 import PropTypes from 'prop-types';
 
 const styles = theme => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-    },
-    gridList: {
-        //width: 500,
-        //height: 450,
-        overflowY: 'auto',
-    },
     logo: {
         height: '250px',
         width: 'auto',
         maxWidth: 'none',
         minWidth: 'none',
     },
-    imgContainer: {
-        overflow: 'hidden',
-        textAlign: 'center',
-    },
-    lineBreak: {
-        whiteSpace: 'pre-line'
-    },
+    // Make sure all cards have same height
     card: {
-        padding: 20,
+        height: '100%'
     },
     media: {
-        height: 250,
-        width: "auto",
-        marginLeft: "auto",
-        marginRight: "auto"
-    }
+        // height: 250,
+        // width: "auto",
+        // marginLeft: "auto",
+        // marginRight: "auto"
+        position: 'absolute',
+        width: '100%',
+        height: '100%'
+    },
+    square: {
+        position: 'relative',
+        // maxWidth: '250px',
+        '&:after': {
+            content: '""',
+            display: 'block',
+            paddingBottom: '100%',
+        }
+    },
 });
 
 class Sponsors extends React.Component {
@@ -44,97 +39,71 @@ class Sponsors extends React.Component {
         super(props);
 
         this.state = {
-            cols: 2,
+            loading: true,
             sponsors: []
         };
-
-        this.onResize = this.onResize.bind(this);
-    }
-
-    onResize() {
-        var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        if (width < 570) {
-            this.setState({cols: 1})
-        } else if(width < 800) {
-            this.setState({cols: 2})
-        } else if (width < 1201) {
-            this.setState({cols: 3})
-        } else if (width < 1601) {
-            this.setState({cols: 4})
-        } else {
-            this.setState({cols: 5})
-        }
-    }
-
-    componentWillMount() {
-        this.onResize();
     }
 
     componentDidMount() {
-        const { classes } = this.props;
-
-        window.addEventListener("resize", this.onResize);
-        fetch("https://kpopplanet-dev.azurewebsites.net/Sponsors/Get")
+        // fetch("https://kpopplanet-dev.azurewebsites.net/Sponsors/Get")
+        fetch("http://localhost:3001/api/sponsors")
         .then(results => {
             return results.json();
         }).then(data => {
-            let retrievedSponsors = data.map((sponsor) => {
-                sponsor.description = sponsor.description.replace("\\n", '\n')
-                return(
-                    <Card
-                        key={sponsor.name}
-                        // style={styles.card}
-                        className={classes.card}
-                    >
-                        <CardMedia
-                            className={classes.media}
-                            component="img"
-                            image={sponsor.logoUrl}
-                            title={sponsor.name + " Logo"}
-                        >
-                            {/* <div style={styles.imgContainer}>
-                                <img style={styles.logo} src={sponsor.logoUrl} alt={sponsor.name + " Logo"}/>
-                            </div>
-                            <div>
-                                <CardTitle title={sponsor.name} subtitle={sponsor.location} />
-                            </div> */}
-                        </CardMedia>
-                        <Typography variant="h6" component="h2">
-                            {sponsor.name}
-                        </Typography>
-                        <Typography gutterBottom variant="caption" component="p">
-                            {sponsor.location}
-                        </Typography>
-                        <Typography paragraph style={styles.lineBreak}>
-                            {sponsor.description}
-                        </Typography>
-                    </Card>
-                )
-            })
-            this.setState({sponsors: retrievedSponsors});
+            this.setState({sponsors: data});
             // console.log("state", this.state.sponsors);
         })
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.onResize);
+        this.setState({loading: false});
     }
 
     render() {
         const { classes } = this.props;
+        // console.log(this.state.sponsors);
         return(
-            <div className={classes.root}>
-                <GridList
-                    cols={this.state.cols}
-                    cellHeight={'auto'}
-                    padding={1}
-                    spacing={30}
-                    // style={styles.gridList}
-                    className={classes.gridList}
-                >
-                    {this.state.sponsors}
-                </GridList>
-            </div>
+            <Grid container spacing={24} justify="center">
+                <Grid item xs={12}>
+					<Typography variant="h5" align="center" component="h1">
+						Sponsors
+					</Typography>
+				</Grid>
+                {
+                    this.state.loading 
+                    ? <Typography gutterBottom variant="caption" component="p">Loading in progress</Typography> 
+                    : this.state.sponsors === undefined || this.state.sponsors.length == 0
+                        ? <Typography gutterBottom variant="body1" component="p">Unable to load sponsors</Typography>
+                        : this.state.sponsors.map((sponsor) => {
+                        sponsor.description = sponsor.description.replace("\\n", '\n');
+                            return (
+                                <Grid item key={sponsor.name} xs={11} sm={6} md={4} lg={3}>
+                                    <Card
+                                        className={classes.card}
+                                        elevation={3}
+                                    >
+                                        <div className={classes.square}>
+                                            <CardMedia
+                                                className={classes.media}
+                                                component="img"
+                                                image={sponsor.logoUrl}
+                                                title={sponsor.name + " Logo"}
+                                            />
+                                        </div>
+                                        <CardContent>
+                                            <Typography variant="h6" component="h2">
+                                                {sponsor.name}
+                                            </Typography>
+                                            <Typography gutterBottom variant="caption" component="p">
+                                                {sponsor.location}
+                                            </Typography>
+                                            <Typography paragraph style={styles.lineBreak}>
+                                                {sponsor.description}
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            );
+                        })
+                }
+            </Grid>
         );
     }
 }
