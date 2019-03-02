b28fbe103c757866f524fa479d2164c15aa5bc24
Jack Lin <jacklin213@gmail.com>
2019-03-03 10:43:36 +1100

    Add small content ContactUs

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-// import {Tabs, Tab} from 'material-ui/Tabs';
+import Grid from '@material-ui/core/Grid';
 
 import Home from './Home';
 import Sponsors from './Sponsors';
 import AboutUs from './AboutUs';
 import Events from './Events';
-import NotFound from './NotFound';
+import NotFoundError from './error/NotFoundError';
 import ContactUs from './ContactUs';
 
 class Body extends React.Component {     
     render() {
         return(
             <main>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/events" component={Events}/>
-                    <Route path="/sponsors" component={Sponsors}/>
-                    <Route path="/about" component={AboutUs}/>
-                    <Route component={NotFound}/>
-                </Switch>
+                <Grid container spacing={24} justify="center">
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/events" component={Events}/>
+                        <Route path="/sponsors" component={Sponsors}/>
+                        <Route path="/about" component={AboutUs}/>
+                        <Route exact path="/contact" component={ContactUs}/>
+                        <Route component={NotFoundError}/>
+                    </Switch>
+                </Grid>
             </main>
         );
     }
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Grid, Link, Typography } from '@material-ui/core';
 
 class ContactUs extends React.Component {
     constructor(props) {
@@ -7,8 +8,16 @@ class ContactUs extends React.Component {
 
     render() {
         return(
-            <div>
-            </div>
+            <Fragment>
+                <Grid item xs={12}>
+					<Typography gutterBottom variant="h5" align="center" component="h1">
+						Contact Us
+					</Typography>
+                    <Typography paragraph align="center" variant="body1">
+						If you have any questions or enquiries feel free to reach out to us via <Link href="mailto:kpopplanetnz@gmail.com">Email</Link>
+					</Typography>
+				</Grid>
+            </Fragment>
         )
     }
 }
