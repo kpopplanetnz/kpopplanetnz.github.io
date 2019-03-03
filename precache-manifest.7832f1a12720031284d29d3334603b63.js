a94d814527846646aaecc0121c78c210b9af3b4b
Jack Lin <jacklin213@gmail.com>
2019-03-03 20:11:34 +1100

    Build from src

---
+++ precache-manifest.7832f1a12720031284d29d3334603b63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "59747332a7b3e7cf7c7d",
+    "url": "/static/js/main.59747332.chunk.js"
+  },
+  {
+    "revision": "30bc5bb0e05ad722828e",
+    "url": "/static/js/2.30bc5bb0.chunk.js"
+  },
+  {
+    "revision": "59747332a7b3e7cf7c7d",
+    "url": "/static/css/main.d8072114.chunk.css"
+  },
+  {
+    "revision": "788324d58cfc1e7237e995220c239ea0",
+    "url": "/index.html"
+  }
+];
