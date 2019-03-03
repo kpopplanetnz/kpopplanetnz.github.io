69b0922433ca32f6096f1fa245ddfec760df4bbf
Jack Lin <jacklin213@gmail.com>
2019-03-03 20:07:57 +1100

    Fix NotFoundError being wrong sized text

--- src/components/error/NotFoundError.js
+++ src/components/error/NotFoundError.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { Fragment } from 'react';
+import { Grid, Typography } from '@material-ui/core';
 
 function NotFoundError() {
-    return <Typography gutterBottom variant="body1" component="p">Error - Page Not Found</Typography>;
+    return (
+        <Fragment>
+            <Typography variant="h3" component="h1" style={{textAlign: "center"}}>Error - Page Not Found</Typography>
+            <Grid container justify="center">
+                <img src="./images/moonie.png" alt="Moonie"></img>
+            </Grid>
+        </Fragment>
+    )
 }
 
 export default NotFoundError;
